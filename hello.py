156e029595a86699e5a82a00e568afdc4c5f04ec
sumit505 <sumitbhosale505@gmail.com>
2021-03-18 21:57:50 +0000

    added new files

---
+++ hello.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")#URL leading to method
+
+def hello(): # Name of the method
+	return("Hello World!") #indent this line
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',port='8080', ssl_context=('cert.pem', 'privkey.pem')) #Run the flask app at port 8080
